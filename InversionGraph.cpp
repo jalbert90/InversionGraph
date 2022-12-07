4a35efe3b4ed2fdba3f650c08d0a58157e9621b4
jacob <jalbert90@gmail.com>
2022-12-06 23:16:38 -0500

    Easy job of reading inputs and creating edges is complete.

--- InversionGraph.cpp
+++ InversionGraph.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+struct edge {
+	int l, r;
+};
 
 void solve() {
 	int n;
 	cin >> n;
 
 	int* p = new int[n];
+	vector<edge> e;
+
+	for (int i = 0; i < n; i++) {
+		cin >> p[i];
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (p[i] > p[j]) {
+				edge edg;
+				edg.l = i;
+				edg.r = j;
+				e.push_back(edg);
+			}
+		}
+	}
+
+	cout << e.size();
 
 	delete[] p;
 }
