f4066a755b202f8a2ae64dcf99f98eceb816e438
jacob <jalbert90@gmail.com>
2022-12-06 22:58:21 -0500

    Add project files.

---
+++ InversionGraph.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+
+	int* p = new int[n];
+
+	delete[] p;
+}
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+}
