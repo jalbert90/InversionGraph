c529fa29e6568e6ee74e5366138658d82d6c97a5
jacob <jalbert90@gmail.com>
2022-12-18 21:41:38 -0500

    Working but too slow.

--- InversionGraph.cpp
+++ InversionGraph.cpp
@@ -4,34 +4,13 @@
 
 using namespace std;
 
-struct edge {
-	int l, r;
-};
-
-void visit(const vector<edge>& e, vector<bool>& v, int n, int start) {
-	v[start] = true;
-	int fnd = start;
-	vector<edge>::const_iterator it;
-	it = find_if(e.begin(), e.end(), [fnd](edge dmy) {return dmy.l == fnd;});
-	int indit = it - e.begin();
-
-	if (it == e.end()) {
-		return;
-	}
-
-	int j = 1;
-	while (indit + j < e.size()) {
-		if (e[indit + j].l == fnd) {
-			visit(e, v, n, e[indit + j].r);
-			j++;
+void explore(const vector<vector<int>>& c, vector<bool>& v, int hub, int& count) {
+	for (int j = 0; j < c[hub].size(); j++) {
+		if (v[c[hub][j]] == false) {
+			v[c[hub][j]] = true;
+			count--;
+			explore(c, v, c[hub][j], count);
 		}
-		else break;
-	}
-
-		int next = e[indit].r;
-
-	if (v[next] == false) {
-		visit(e, v, n, next);
 	}
 }
 
@@ -39,9 +18,9 @@ void solve() {
 	int n;
 	cin >> n;
 
-	int* p = new int[n];
-	vector<edge> e;
+	vector<int> p(n);
 	vector<bool> v(n, false);
+	vector<vector<int>> c(n);
 
 	for (int i = 0; i < n; i++) {
 		cin >> p[i];
@@ -50,31 +29,22 @@ void solve() {
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1; j < n; j++) {
 			if (p[i] > p[j]) {
-				edge edg;
-				edg.l = i;
-				edg.r = j;
-				e.push_back(edg);
-				edg.l = j;
-				edg.r = i;
-				e.push_back(edg);
+				c[i].push_back(j);
+				c[j].push_back(i);
 			}
 		}
 	}
 
-	sort(e.begin(), e.end(), [](edge a, edge b) { return a.l < b.l; });
-
-	int count = 0;
+	int count = n;
 
 	for (int i = 0; i < n; i++) {
 		if (v[i] == false) {
-			visit(e, v, n, i);
-			count++;
+			v[i] = true;
+			explore(c, v, i, count);
 		}
 	}
 
 	cout << count << endl;
-
-	delete[] p;
 }
 
 int main() {
